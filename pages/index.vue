45f91e05af6f99d597a262b8089c39d03741df3a
elgun_mammadli <elgun@jis.az>
2023-12-18 13:46:35 +0400

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,6 @@
 <template>
 <div>
-    <AppHeader :posts="true"  />
-<!--                <div v-if="data?.new_posts" class="col-md-10 col-lg-8 col-xl-7">-->
-<!--                    &lt;!&ndash; Post preview&ndash;&gt;-->
-<!--                    <div v-for="item of data?.new_posts" :key="item.id" class="post-preview">-->
-<!--                        <NuxtLink :to="'blogs/'+item.id">-->
-<!--                            <h2 class="post-title">{{ item.title }}</h2>-->
-<!--                            &lt;!&ndash; <h3 class="post-subtitle" v-html="item.blogcontent"> </h3> &ndash;&gt;-->
-<!--                        </NuxtLink>-->
-<!--                        <p class="post-meta">-->
-<!--                            Posted by-->
-<!--                            <a target="_blank" href="https://www.linkedin.com/in/elgun-mammadli-b4b2661a9/">Elgun Mammadli</a>-->
-<!--                            {{ item.date }}-->
-<!--                        </p>-->
-<!--                        <hr class="my-4" />-->
-<!--                    </div>-->
-<!--                    &lt;!&ndash; Divider&ndash;&gt;-->
-<!--                    &lt;!&ndash; <hr class="my-4" /> &ndash;&gt;-->
-<!--                    &lt;!&ndash; Pager&ndash;&gt;-->
-<!--                    &lt;!&ndash; <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div> &ndash;&gt;-->
-<!--                </div>-->
+    <AppHeader :posts="true" />
   <div class="container">
     <div class="row" v-if="data?.new_posts">
               <BlogCard v-for="item of data?.new_posts" :key="item.id" :item="item"/>
@@ -27,6 +8,7 @@
   </div>
 </div>
 </template>
+
 
 <script setup lang="ts">
 
@@ -40,12 +22,6 @@ definePageMeta({
 const runtimeConfig = useRuntimeConfig();
 const base_url = runtimeConfig.public.apiUrl;
 
-// const { data, pending, error, refresh } = await useAsyncData(
-//   'mountains',
-//   () => $fetch(base_url + 'data'),{
-//     server : false
-//   }
-// );
 
 const {data, error} = await useAsyncData(
     'posts',
